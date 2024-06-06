974e18f4dda28cdbc030f29dea77ea09aa585cee
Alvaro Resplandor <alvarojrr79@gmail.com>
2024-06-05 22:00:00 -0400

    chore: setup prettier and eslint

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: [
+    "prettier-plugin-astro",
+    "prettier-plugin-tailwindcss",
+    "prettier-plugin-astro-organize-imports",
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  useTabs: true,
+  tabWidth: 2,
+};
