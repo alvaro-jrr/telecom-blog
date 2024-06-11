7e15883fb34b9f1dd01d0a3dad2a44e5b68fc130
Alvaro Resplandor <alvarojrr79@gmail.com>
2024-06-10 22:22:27 -0400

    fix: set site url through env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { loadEnv } from "vite";
 
 import tailwind from "@astrojs/tailwind";
 
+const { SITE_URL = "" } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: SITE_URL,
 	integrations: [mdx(), sitemap(), tailwind()],
 });
